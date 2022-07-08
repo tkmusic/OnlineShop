72310d51f751652d4d719166f0791331ffff38a2
tkmusic <68860474+tkmusic@users.noreply.github.com>
2022-07-08 17:45:06 +0200

    Added Client-Side functionality to delete ShoppingCartItems

--- OnlineShop.Web/Services/Contracts/IShoppingCartService.cs
+++ OnlineShop.Web/Services/Contracts/IShoppingCartService.cs
@@ -4,6 +4,7 @@ namespace OnlineShop.Web.Services.Contracts;
 
 public interface IShoppingCartService
 {
-    Task<IEnumerable<CartItemDto>> GetItems(int userId);
+    Task<List<CartItemDto>> GetItems(int userId);
     Task<CartItemDto> AddItem(CartItemToAddDto cartItemToAddDto);
+    Task<CartItemDto> DeleteItem(int id);
 }
--- OnlineShop.Web/Services/ShoppingCartService.cs
+++ OnlineShop.Web/Services/ShoppingCartService.cs
@@ -13,7 +13,7 @@ public class ShoppingCartService : IShoppingCartService
         _httpClient = httpClient;
     }
     
-    public async Task<IEnumerable<CartItemDto>> GetItems(int userId)
+    public async Task<List<CartItemDto>> GetItems(int userId)
     {
         try
         {
@@ -23,10 +23,10 @@ public class ShoppingCartService : IShoppingCartService
             {
                 if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
                 {
-                    return Enumerable.Empty<CartItemDto>();
+                    return Enumerable.Empty<CartItemDto>().ToList();
                 }
 
-                return (await response.Content.ReadFromJsonAsync<IEnumerable<CartItemDto>>())!;
+                return (await response.Content.ReadFromJsonAsync<List<CartItemDto>>())!;
             }
             else
             {
@@ -66,5 +66,22 @@ public class ShoppingCartService : IShoppingCartService
         }
     }
 
-    
+    public async Task<CartItemDto> DeleteItem(int id)
+    {
+        try
+        {
+            var response = await _httpClient.DeleteAsync($"api/ShoppingCart/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<CartItemDto>();
+            }
+
+            return default(CartItemDto);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 }
